60827cecfe52e115970aa4db7975a05005dbcfcb
Nofre <davidprogram417@gmail.com>
2025-09-08 20:57:34 +0200

    Add placeholder WordPress connection test response
    - Introduced a temporary `TestConnectionResponse` in `handlers/site.go` to always return a successful `connected` status.
    - Added a TODO comment to implement actual WordPress connection testing logic in the future.

--- internal/handlers/site.go
+++ internal/handlers/site.go
@@ -181,6 +181,7 @@ func (h *Handler) TestSiteConnection(req dto.TestSiteConnectionRequest) (*dto.Te
 
 	// For now, return a successful connection test
 	// TODO: Implement actual WordPress connection testing with site data
+
 	response := &dto.TestConnectionResponse{
 		Success:   true,
 		Status:    "connected",
