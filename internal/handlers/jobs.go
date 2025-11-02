88e08106dc9b368f93a1b71210b4f7e8f80d1527
Nofre <davidprogram417@gmail.com>
2025-11-03 00:54:40 +0100

    Introduce `JobsHandler` for managing job-related HTTP operations
    - Added `JobsHandler` with comprehensive job management functionalities (CRUD, pause, resume, execute).
    - Introduced methods for converting entities to DTOs for consistent data handling.
    - Improved modular design by centralizing job-related logic within a dedicated handler.

--- internal/handlers/jobs.go
+++ internal/handlers/jobs.go
@@ -1,1 +1,98 @@
 package handlers
+
+import (
+	"github.com/davidmovas/postulator/internal/domain/jobs"
+	"github.com/davidmovas/postulator/internal/dto"
+	"github.com/davidmovas/postulator/pkg/ctx"
+)
+
+type JobsHandler struct {
+	service jobs.Service
+}
+
+func NewJobsHandler(service jobs.Service) *JobsHandler {
+	return &JobsHandler{
+		service: service,
+	}
+}
+
+func (h *JobsHandler) CreateJob(job *dto.Job) *dto.Response[string] {
+	entity, err := job.ToEntity()
+	if err != nil {
+		return fail[string](err)
+	}
+
+	if err = h.service.CreateJob(ctx.FastCtx(), entity); err != nil {
+		return fail[string](err)
+	}
+
+	return ok("Job created successfully")
+}
+
+func (h *JobsHandler) GetJob(id int64) *dto.Response[*dto.Job] {
+	job, err := h.service.GetJob(ctx.FastCtx(), id)
+	if err != nil {
+		return fail[*dto.Job](err)
+	}
+
+	return ok(dto.NewJob(job))
+}
+
+func (h *JobsHandler) ListJobs() *dto.Response[[]*dto.Job] {
+	listJobs, err := h.service.ListJobs(ctx.FastCtx())
+	if err != nil {
+		return fail[[]*dto.Job](err)
+	}
+
+	var dtoJobs []*dto.Job
+	for _, job := range listJobs {
+		dtoJobs = append(dtoJobs, dto.NewJob(job))
+	}
+
+	return ok(dtoJobs)
+}
+
+func (h *JobsHandler) UpdateJob(job *dto.Job) *dto.Response[string] {
+	entity, err := job.ToEntity()
+	if err != nil {
+		return fail[string](err)
+	}
+
+	if err = h.service.UpdateJob(ctx.FastCtx(), entity); err != nil {
+		return fail[string](err)
+	}
+
+	return ok("Job updated successfully")
+}
+
+func (h *JobsHandler) DeleteJob(id int64) *dto.Response[string] {
+	if err := h.service.DeleteJob(ctx.FastCtx(), id); err != nil {
+		return fail[string](err)
+	}
+
+	return ok("Job deleted")
+}
+
+func (h *JobsHandler) PauseJob(id int64) *dto.Response[string] {
+	if err := h.service.PauseJob(ctx.FastCtx(), id); err != nil {
+		return fail[string](err)
+	}
+
+	return ok("Job paused")
+}
+
+func (h *JobsHandler) ResumeJob(id int64) *dto.Response[string] {
+	if err := h.service.ResumeJob(ctx.FastCtx(), id); err != nil {
+		return fail[string](err)
+	}
+
+	return ok("Job resumed")
+}
+
+func (h *JobsHandler) ExecuteManually(jobID int64) *dto.Response[string] {
+	if err := h.service.ExecuteManually(ctx.FastCtx(), jobID); err != nil {
+		return fail[string](err)
+	}
+
+	return ok("Job executed")
+}
