b9d2f4f52f930d708aefacd409e1f1437ef372f1
Nofre <davidprogram417@gmail.com>
2025-11-03 02:06:53 +0100

    Refactor `CheckHealth` to use Resty client for simplified HTTP handling

--- internal/infra/wp/health.go
+++ internal/infra/wp/health.go
@@ -10,25 +10,16 @@ import (
 )
 
 func (c *restyClient) CheckHealth(ctx context.Context, site *entities.Site) (entities.HealthStatus, error) {
-	endpoint := c.getAPIURL(site.URL, "")
-
-	req, err := http.NewRequestWithContext(ctx, "GET", endpoint, nil)
+	resp, err := c.resty.R().
+		SetContext(ctx).
+		SetBasicAuth(site.WPUsername, site.WPPassword).
+		Get(c.getAPIURL(site.URL, ""))
 	if err != nil {
 		return entities.HealthUnknown, errors.WordPress("error while requesting", err)
 	}
 
-	c.setAppPasswordAuth(req, site.WPUsername, site.WPPassword)
-
-	resp, err := c.httpClient.Do(req)
-	if err != nil {
-		return entities.HealthUnknown, errors.WordPress("error while requesting", err)
-	}
-	defer func() {
-		_ = resp.Body.Close()
-	}()
-
-	if resp.StatusCode != http.StatusOK {
-		return entities.HealthUnhealthy, errors.WordPress(fmt.Sprintf("WordPress API respond with code: %d", resp.StatusCode), nil)
+	if resp.StatusCode() != http.StatusOK {
+		return entities.HealthUnhealthy, errors.WordPress(fmt.Sprintf("WordPress API responded with code: %d", resp.StatusCode()), nil)
 	}
 
 	return entities.HealthHealthy, nil
